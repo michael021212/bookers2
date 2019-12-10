9b669f827afe10143e8f33e4fd6a1a79f5829fea
tanakashun <michael021212@gmail.com>
2019-12-10 17:44:37 +0000

    userEdit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'users#show'
+  # get 'users/show'
+  devise_for :users
+  resources :users, only: [:show, :edit]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
