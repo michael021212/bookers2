39e0e69b977068d84a6e2c1a96899c97714789e6
tanakashun <michael021212@gmail.com>
2020-01-29 14:08:24 +0900

    [add]postcode&map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
- 
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:postcode,:prefecture_code,:address_city,:address_street,:address_building])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
